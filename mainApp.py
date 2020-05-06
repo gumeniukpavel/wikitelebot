f961363f1342d288c3df83c9cd8c8058aae5d7c5
Paul Gumeniuk <g.pasha@email.ua>
2020-05-06 21:33:49 +0300

    Change Lang

--- mainApp.py
+++ mainApp.py
@@ -2,7 +2,9 @@ from telebot import types, TeleBot
 import wikipedia
 
 bot = TeleBot('899161541:AAFTse3SCtDR1P0tN-s8mLpgbXdd7t-mud4')
-wikipedia.set_lang('uk')
+
+lang = 'uk'
+lang_mess = 'українська'
 
 
 @bot.message_handler(commands=['start'])
@@ -10,8 +12,41 @@ def start_message(message):
     bot.send_message(message.chat.id, 'Введи свій пошуковий запит', )
 
 
+@bot.message_handler(commands=['changelang'])
+def change_lang_message(message):
+    keyboard = types.InlineKeyboardMarkup()
+    key_uk = types.InlineKeyboardButton(text='Українська', callback_data='uk')
+    keyboard.add(key_uk)
+    key_ru = types.InlineKeyboardButton(text='Русский', callback_data='ru')
+    keyboard.add(key_ru)
+    key_en = types.InlineKeyboardButton(text='English', callback_data='en')
+    keyboard.add(key_en)
+    bot.send_message(message.chat.id, 'Виберіть мову пошуку', reply_markup=keyboard)
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_worker(call):
+    global lang
+    lang = call.data
+    bot.send_message(call.message.chat.id, 'Мову пошуку змінено', )
+
+
+@bot.message_handler(commands=['getlang'])
+def change_lang_message(message):
+    global lang_mess
+    if lang == 'uk':
+        lang_mess = 'українська'
+    if lang == 'ru':
+        lang_mess = 'російська'
+    if lang == 'en':
+        lang_mess = 'англійська'
+
+    bot.send_message(message.chat.id, 'Ваша мова пошуку ' + lang_mess)
+
+
 @bot.message_handler(content_types=['text'])
 def send_text(message):
+    wikipedia.set_lang(lang)
     keyboard = types.ReplyKeyboardMarkup(True, True)
     search_results = wikipedia.search(message.text)
     if search_results:
